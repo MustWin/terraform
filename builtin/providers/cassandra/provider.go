43eaad66fd6dd50e8e0dfe7e75db86bcb31de1bf
Mike Ihbe <mikejihbe@gmail.com>
2016-06-11 00:06:58 -0700

    Start to cassandra providers code, based on influxdb provider

---
+++ builtin/providers/cassandra/provider.go
@@ -0,0 +1,70 @@
+package cassandra
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/gocql/gocql"
+)
+
+var quoteReplacer = strings.NewReplacer(`"`, `\"`)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"cassandra_keyspace": ResourceKeyspace(),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"hostport": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.EnvDefaultFunc(
+					"CASSANDRA_HOSTPORT", "localhost:9042",
+				),
+			},
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CASSANDRA_USERNAME", ""),
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CASSANDRA_PASSWORD", ""),
+			},
+		},
+
+		ConfigureFunc: Configure,
+	}
+}
+
+func Configure(d *schema.ResourceData) (interface{}, error) {
+	hostPortRegex, err := regexp.Compile(".*:\\d*")
+	if err != nil {
+		return nil, fmt.Errorf("Invalid regex: %s", err)
+	}
+
+	hostPort := d.Get("hostport").(string)
+	if !hostPortRegex.MatchString(hostPort) {
+		return nil, fmt.Errorf("invalid Cassandra Hostport: %s", err)
+	}
+
+	cluster := gocql.NewCluster(hostPort)
+	cluster.ProtoVersion = 1
+	cluster.Keyspace = "system"
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func quoteIdentifier(ident string) string {
+	return fmt.Sprintf(`"%s"`, quoteReplacer.Replace(ident))
+}
---
+++ builtin/providers/cassandra/resource_database.go
@@ -0,0 +1,138 @@
+package cassandra
+
+import (
+	"fmt"
+
+	"github.com/gocql/gocql"
+	"github.com/hashicorp/terraform/helper/schema"
+	"strings"
+)
+
+const (
+	ReplicationStrategySimple          = "SimpleStrategy"
+	ReplicationStrategyNetworkTopology = "NetworkTopologyStrategy"
+)
+
+func ResourceKeyspace() *schema.Resource {
+	return &schema.Resource{
+		Create: CreateKeyspace,
+		Read:   ReadKeyspace,
+		Delete: DeleteKeyspace,
+
+		// https://docs.datastax.com/en/cql/3.1/cql/cql_reference/create_keyspace_r.html
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true, // TODO: investigate what this does
+			},
+			"durable_writes": &schema.Schema{
+				Type:     schema.TypeBool,
+				Required: true,
+				ForceNew: true,
+			},
+			"replication_class": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			// Required if replication_class == "SimpleStrategy"
+			"replication_factor": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: false,
+				ForceNew: true,
+			},
+			// Required if replication_class == "NetworkTopologyStrategy"
+			"datacenters": &schema.Schema{
+				Type:     schema.TypeMap,
+				Required: false,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func CreateKeyspace(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*gocql.Session)
+
+	name := d.Get("name").(string)
+	queryParams := make([]interface{}, 0)
+	queryParams = append(queryParams, name)
+	replicationStr := "{ 'class': ?"
+	replicationClass := d.Get("replication_class").(string)
+	if replicationClass != ReplicationStrategySimple && replicationClass != ReplicationStrategyNetworkTopology {
+		return fmt.Errorf("replication_class must be one of [%s, %s]", ReplicationStrategySimple, ReplicationStrategyNetworkTopology)
+	}
+
+	switch replicationClass {
+	case ReplicationStrategySimple:
+		queryParams = append(queryParams, replicationClass)
+		replicationFactor := d.Get("replication_factor").(string)
+		replicationStr += ", replication_factor: ? }"
+		queryParams = append(queryParams, replicationFactor)
+	case ReplicationStrategyNetworkTopology:
+		queryParams = append(queryParams, replicationClass)
+		datacenters := d.Get("datacenters").(map[string]interface{})
+		for datacenter, count := range datacenters {
+			replicationStr += ", ?: ?"
+			queryParams = append(queryParams, datacenter)
+			queryParams = append(queryParams, count)
+		}
+		replicationStr += " }"
+	}
+	queryParams = append(queryParams, d.Get("durable_writes").(bool))
+
+	err := conn.Query("CREATE KEYSPACE IF NOT EXIST ? WITH REPLICATION = " + replicationStr + " AND DURABLE_WRITES = ?").Exec()
+	if err != nil {
+		return err
+	}
+
+	d.SetId(name)
+
+	return nil
+}
+
+func ReadKeyspace(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*gocql.Session)
+	name := d.Id()
+
+	iter := conn.Query("SELECT keyspace_name FROM system.schema_keyspaces", name).Iter()
+	var keyspace string
+	found := false
+	for iter.Scan(&keyspace) {
+		if keyspace == name {
+			found = true
+		}
+	}
+	err := iter.Close()
+	if err != nil {
+		return err
+	}
+
+	if !found {
+		d.SetId("")
+	}
+
+	return nil
+}
+
+func DeleteKeyspace(d *schema.ResourceData, meta interface{}) error {
+	err := ReadKeyspace(d, meta)
+	if err != nil {
+		return err
+	}
+
+	conn := meta.(*gocql.Session)
+	name := d.Id()
+
+	if d.Id() {
+		err := conn.Query("DROP KEYSPACE ?", name).Exec()
+		if err != nil {
+			return err
+		}
+		d.SetId("")
+
+	}
+
+	return nil
+}
