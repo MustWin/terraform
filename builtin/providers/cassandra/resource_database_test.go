ffafba61bc2bf7ba4e544c1ee08725ed177d4b0f
Mike Ihbe <mikejihbe@gmail.com>
2016-06-11 16:10:17 -0700

    Add basics of an actual acceptance test that interacts with cassandra

--- builtin/providers/cassandra/resource_database_test.go
+++ builtin/providers/cassandra/resource_database_test.go
@@ -3,21 +3,31 @@ package cassandra
 import (
 	"testing"
 
+	"fmt"
+	"github.com/gocql/gocql"
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"log"
+	"strings"
 )
 
 func TestSimpleReplicationDatabase(t *testing.T) {
+	var keyspaceDesc string
+
 	resource.Test(t, resource.TestCase{
-		Providers: testProviders,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccDatabaseConfig,
 				Check: resource.ComposeTestCheckFunc(
+					checkKeyspaceExists("terraform-test", &keyspaceDesc),
+					checkKeyspaceProperties(keyspaceDesc, "'replication_factor': '2'"),
 					resource.TestCheckResourceAttr(
 						"cassandra_keyspace.test", "name", "terraform-test",
 					),
 					resource.TestCheckResourceAttr(
-						"cassandra_keyspace.test", "durable_writes", "1",
+						"cassandra_keyspace.test", "durable_writes", "5", // TODO: This should fail, why doesn't it?
 					),
 					resource.TestCheckResourceAttr(
 						"cassandra_keyspace.test", "replication_class", ReplicationStrategySimple,
@@ -28,12 +38,40 @@ func TestSimpleReplicationDatabase(t *testing.T) {
 	})
 }
 
-var testAccDatabaseConfig = `
+func checkKeyspaceExists(name string, keyspaceDesc *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*gocql.Session)
+		log.Println("DESCRIBING KEYSPACE")
+		err := conn.Query("DESCRIBE KEYSPACE ?", name).Scan(keyspaceDesc)
+		if err != nil {
+			log.Println("Returning error: %s", err)
+			return err
+		}
+		return nil
+	}
+}
+
+func checkKeyspaceProperties(keyspaceDesc string, stringsToMatch ...string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, match := range stringsToMatch {
+			if !strings.Contains(keyspaceDesc, match) {
+				return fmt.Errorf("Keyspace description did not match expected string: `%s`", match)
+			}
+		}
+
+		return nil
+	}
+}
+
+const (
+	testAccDatabaseConfig = `
 
 resource "cassandra_keyspace" "test" {
     name = "terraform-test"
     durable_writes = 1
     replication_class = "` + ReplicationStrategySimple + `"
+    replication_factor = 2
 }
 
 `
+)
